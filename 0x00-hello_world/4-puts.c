753400e338288705174efa6562ba5d21846a146a
hakeem-hassan <hlanre@ymail.com>
2023-07-14 00:01:20 +0000

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
-#include <studio.h>
+#include <stdio.h>
 /**
  * main - A program that prints a line with put function 
  * Return: 0 (success)
  */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
