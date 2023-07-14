1068f9a5ba27eb2cacb205cabcc6968171851d8f
hakeem-hassan <hlanre@ymail.com>
2023-07-14 02:17:13 +0000

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,8 +2,8 @@
 
 
 /**
- * main - Prints the size of various types on the 
- * computer it is compiled and run on ..
+ * main - Prints the size of various typesa based on 
+ * the computer it is compiled and run on ..
  * Return: Always 0.
  */
 
