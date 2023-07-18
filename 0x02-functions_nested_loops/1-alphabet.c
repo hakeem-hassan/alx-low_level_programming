2c92cf8a7ffae3a3ee835919bf392a4cff398d6f
hakeem-hassan <hlanre@ymail.com>
2023-07-18 13:58:27 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,11 @@
 * Return: void
 */
 
-  void print_alphabet(void)
-  {
-   char letter;
-   for (letter = 'a'; letter <= 'z'; letter++)
-   _putchar(letter);
-   _putchar('\n');
-   }
+ void print_alphabet(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
 
