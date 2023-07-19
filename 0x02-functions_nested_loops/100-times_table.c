11053e265f5cb6e348f886973ce8132b1ae63bc9
hakeem-hassan <hlanre@ymail.com>
2023-07-18 18:58:37 -0700

    100timestable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,16 +10,22 @@ void print_times_table(int n)
 int i, j, k;
 
 if (n >= 0 && n <= 15)
+
 {
 for (i = 0; i <= n; i++)
+
 {
 for (j = 0; j <= n; j++)
+
 {
     k = j * i;
+
 if (j == 0)
+
 {
       _putchar(k + '0');
 } 
+
 else if (k < 10 && j != 0)
 {
  _putchar(',');
@@ -27,8 +33,10 @@ else if (k < 10 && j != 0)
  _putchar(' ');
  _putchar(' ');
  _putchar(k + '0');
-} 
+}
+
 else if (k >= 10 && k < 100)
+
 {
 _putchar(',');
 _putchar(' ');
@@ -36,13 +44,15 @@ _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
 }
+
 else if (k >= 100)
+
 { 
-       _putchar(',');
-       _putchar(' ');
-       _putchar((k / 100) + '0');
-       _putchar(((k / 10) % 10) + '0');
-       _putchar((k % 10) + '0');
+_putchar(',');
+_putchar(' ');
+_putchar((k / 100) + '0');
+_putchar(((k / 10) % 10) + '0');
+_putchar((k % 10) + '0');
 }
 
 }
@@ -51,4 +61,5 @@ else if (k >= 100)
 }
 
 }
+
 }
