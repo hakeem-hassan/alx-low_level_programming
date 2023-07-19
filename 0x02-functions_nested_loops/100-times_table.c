9f0b60889d5a08bd7784be211722688099754532
hakeem-hassan <hlanre@ymail.com>
2023-07-18 18:46:07 -0700

    100timestable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,9 +17,7 @@ for (j = 0; j <= n; j++)
 {
            k = j * i;
 if (j == 0)
-
 {
-
       _putchar(k + '0');
 } 
 else if (k < 10 && j != 0)
@@ -31,21 +29,20 @@ else if (k < 10 && j != 0)
       _putchar(k + '0');
 } 
 else if (k >= 10 && k < 100)
-
 {
-	    _putchar(',');
-             _putchar(' ');
-             _putchar(' ');
-             _putchar((k / 10) + '0');
-             _putchar((k % 10) + '0');
+	_putchar(',');
+        _putchar(' ');
+        _putchar(' ');
+        _putchar((k / 10) + '0');
+        _putchar((k % 10) + '0');
 }
 else if (k >= 100)
 { 
-              _putchar(',');
-              _putchar(' ');
-              _putchar((k / 100) + '0');
-              _putchar(((k / 10) % 10) + '0');
-              _putchar((k % 10) + '0');
+       _putchar(',');
+       _putchar(' ');
+       _putchar((k / 100) + '0');
+       _putchar(((k / 10) % 10) + '0');
+       _putchar((k % 10) + '0');
 }
 
 }
