76a4216a3a72019a4a27584bd69eaf7995a9e39d
hakeem-hassan <hlanre@ymail.com>
2023-07-18 18:49:01 -0700

    100timestable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,11 +22,11 @@ if (j == 0)
 } 
 else if (k < 10 && j != 0)
 {
-      _putchar(',');
-      _putchar(' ');
-      _putchar(' ');
-      _putchar(' ');
-      _putchar(k + '0');
+ _putchar(',');
+ _putchar(' ');
+ _putchar(' ');
+ _putchar(' ');
+ _putchar(k + '0');
 } 
 else if (k >= 10 && k < 100)
 {
