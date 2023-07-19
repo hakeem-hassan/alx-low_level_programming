4cf226153201019717e3161f72cc3d6b5cd994c1
hakeem-hassan <hlanre@ymail.com>
2023-07-18 19:20:21 -0700

    100timestable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,9 +18,7 @@ k = j * i;
 if (j == 0)
 {
 _putchar(k + '0');
-
 }
-
 else if (k < 10 && j != 0)
 {
 _putchar(',');
@@ -38,7 +36,8 @@ _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
 }
 else if (k >= 100)
-{ 
+{
+
 _putchar(',');
 _putchar(' ');
 _putchar((k / 100) + '0');
